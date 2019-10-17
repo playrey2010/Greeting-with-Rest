880fa54bb266cc5fe0dd5fe37d9f335268868288
playrey2010 <playrey2010@gmail.com>
2019-10-17 00:12:07 -0400

    initial commit, simple restapp

---
+++ src/test/java/hello/GreetingApplicationTests.java
@@ -0,0 +1,14 @@
+package hello;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreetingApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
